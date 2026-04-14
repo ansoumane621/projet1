804218841767c670755e27753637dcae8d70b1ec
ANSOUMANE CONDE <ansoumaneconde512@gmail.com>
2026-04-14 10:27:30 +0000

    save local changes

--- app.py
+++ app.py
@@ -258,4 +258,5 @@ if __name__ == '__main__':
     print("\nStarting server...")
     print("Open http://127.0.0.1:5000 in your browser\n")
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=7860)
